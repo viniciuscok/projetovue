7e5b8f755485b693535ba20e606be58743cf7cda
Vinicius <vinicius_cok@hotmail.com>
2019-11-06 21:53:19 -0300

    Mudança de API

--- src/resources/index.js
+++ src/resources/index.js
@@ -3,8 +3,8 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource)
 
-//Vue.http.options.root = 'https://app-4neo.herokuapp.com';
-Vue.http.options.root = 'http://localhost:8081';
+Vue.http.options.root = 'https://app-4neo.herokuapp.com';
+//Vue.http.options.root = 'http://localhost:8081';
 Vue.prototype.$api = {
     cidades: Vue.resource('cidades{/id}'),
     estados: Vue.resource('estados{/id}'),
